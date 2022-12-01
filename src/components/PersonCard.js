9c96d20a7f819a3fd26c1aff22a58eaee7505033
Julian Victoriano <jvictoriano35@yahoo.com>
2022-11-30 19:00:33 -0800

    assingment

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const PersonCard = (props) => {
+    const {firstName, lastName, age, hairColor } = props;
+    const [ stateAge, setStateAge ] = useState(age);
+    const handleClick = () => {
+        setStateAge( stateAge + 1 )
+    }
     return (
         <div>
-            <h1>{ props.lastName }, { props.firstName }</h1>
-            <p>Age: { props.age }</p>
-            <p>Hair Color: { props.hairColor }</p>
+            <h1>{ lastName }, { firstName }</h1>
+            <p>Age: { stateAge }</p>
+            <p>Hair Color: { hairColor }</p>
+            <button onClick={ handleClick }> Birthday Button for {firstName} {lastName} </button>
         </div>
     )
 }
