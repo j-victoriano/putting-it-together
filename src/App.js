5c396d3df53d194fee3c491d49ce75eb9ee61698
Julian Victoriano <jvictoriano35@yahoo.com>
2022-11-30 16:51:29 -0800

    assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import PersonCard from './components/PersonCard';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PersonCard 
+      firstName={"Jane"}
+      lastName={"Doe"}
+      age={45}
+      hairColor={"Black"}/>
+      <br/>
+      <PersonCard 
+      firstName={"John"}
+      lastName={"Smith"}
+      age={88}
+      hairColor={"Brown"}/>
+      <br/>
+      <PersonCard 
+      firstName={"Millard"}
+      lastName={"Fillmore"}
+      age={50}
+      hairColor={"Brown"}/>
+      <br/>
+      <PersonCard 
+      firstName={"Maria"}
+      lastName={"Smith"}
+      age={62}
+      hairColor={"Brown"}/>
+      <br/>
     </div>
   );
 }
